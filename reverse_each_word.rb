0bd8df0b8676ff852e553c7a79b62cfa094f38e3
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 11:50:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(all_words)
-  all_words.split.each { |i| puts i.reverse!, ""}.join(" ")
+  all_words.collect { |z| z.reverse! }.join(" ")
+  return all_words.split.each { |i| puts i.reverse!, ""}.join(" ")
 end
 
-def reverse_each_word_two (all_words)
-  all_words.collect { |z| z.reverse! }.join(" ")
-end
